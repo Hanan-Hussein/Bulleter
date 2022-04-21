183d45fe365ec2e2ee97361c136de6998869622b
Jaffar Hussein <jaffhussein@icloud.com>
2022-04-21 13:16:16 +0200

    rf:refactored the code

--- bulleter.py
+++ bulleter.py
@@ -1,12 +1,12 @@
-
 import pyperclip
 
 
-def bulleter_function(lines:list)->str:
+def bulleter_function(lines: list) -> str:
     """
     This functiontakes in a list of lines andd
     capitalizes the first character of each line
     adds * to the beginning at the beginning of the same line
+    Also adds a space between the lines for readability
 
     Args:
         line (string): This is the string that represents a line of the document
@@ -17,28 +17,27 @@ def bulleter_function(lines:list)->str:
     newfile = ""
     for line in lines:
         newfile += f"* {line.capitalize()} \n"
-    
+
     return newfile
 
 
-def exporter_clipboarder(txt_file:str,file_name:str)->None:
+def exporter_clipboarder(txt_file: str, file_name: str) -> None:
     """
-    Writes a file into a new file which has been named by
-    the file name
-
+    Creates a file with the name  `file_name`
+    Writes a some text file into the created file.
+    Copies the contents of the file onto a clipboard.
+    
     Args:
         txt_file (str): The file that has to be written
         file_name (str): The name of the destination file to be written
     """
-    with open(f'{file_name}.txt','w') as file_name:
-        print(txt_file,file=file_name)
-    pyperclip.copy(file_name)
-    
+    with open(f'{file_name}.txt', 'w') as file_name:
+        print(txt_file, file=file_name)
+    # pyperclip.copy(file_name)
 
 
-with open('text.txt','r') as text:
+# Test Case
+with open('text.txt', 'r') as text:
     lines = text.readlines()
     new_file = bulleter_function(lines)
-    exporter_clipboarder(new_file,"beauty")
-    
-    
+    exporter_clipboarder(new_file, "beauty")
